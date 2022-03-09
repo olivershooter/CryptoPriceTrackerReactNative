dabb6a3ef1417435f05d2a2d37d85a5e7c9b1c2e
OXS090 <olivershooter@gmail.com>
2022-03-09 16:58:07 +0000

    update

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.coinContainer}>
+        <Image
+          source={{ uri: "https://i.imgur.com/S1zYdCv.png" }}
+          style={{
+            height: 30,
+            width: 30,
+            marginRight: 10,
+            alignSelf: "center",
+          }}
+        />
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.text}>10</Text>
+            <Text style={styles.text}>BTC</Text>
+            <Text style={styles.text}>0.61%</Text>
+          </View>
+        </View>
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +31,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#121212",
+    paddingTop: 50,
+  },
+
+  coinContainer: {
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "#282828",
+    padding: 15,
+  },
+
+  title: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  text: {
+    color: "white",
+    fontSize: 16,
+    marginRight: 5,
   },
 });
---
+++ README.md
@@ -0,0 +1,26 @@
+  <h3 align="center">Crypto Tracker - React Native</h3>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+Built following along with the wonderful [notJust.dev](https://www.youtube.com/channel/UCYSa_YLoJokZAwHhlwJntIA). Their Youtube video [here](https://www.youtube.com/watch?v=K6i02mJc8Zc)
+
+### Built With
+
+- [React Native](https://reactnative.dev/)
+- [Expo Go](https://expo.dev/client)
+
+## Getting Started
+
+Simply fork or clone the repo.
+
+## Future improvements
+
+-
+
+<!-- CONTACT -->
+
+## Contact
+
+[Oliver Shooter](https://www.olivershooter.me/)
