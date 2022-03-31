f746f388751b14978e3d10ad65941487a2bffc36
OXS090 <olivershooter@gmail.com>
2022-03-31 17:19:51 +0100

    part 4 done

--- src/screens/AddNewAssetScreen/index.jsx
+++ src/screens/AddNewAssetScreen/index.jsx
@@ -8,6 +8,7 @@ import { useRecoilState } from "recoil";
 import { allPortfolioBoughtAssetsInStorage } from "../../atoms/PortfolioAssets";
 import { getAllCoins, getDetailedCoinData } from "../../services/requests";
 import { useNavigation } from "@react-navigation/native";
+import uuid from "react-native-uuid";
 
 const AddNewAssetScreen = () => {
   const [allCoins, setAllCoins] = useState([]);
@@ -56,6 +57,7 @@ const AddNewAssetScreen = () => {
   const onAddNewAsset = async () => {
     const newAsset = {
       id: selectedCoin.id,
+      unique_id: selectedCoin.id + uuid.v4(),
       name: selectedCoin.name,
       image: selectedCoin.image.small,
       ticker: selectedCoin.symbol.toUpperCase(),
--- src/screens/PortfolioScreen/components/PortfolioAssetsList/index.jsx
+++ src/screens/PortfolioScreen/components/PortfolioAssetsList/index.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { View, Text, FlatList, Pressable } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AntDesign } from "@expo/vector-icons";
 import styles from "./styles.js";
 import PortfolioAssetItem from "../PortfolioAssetItem";
 import { useNavigation } from "@react-navigation/native";
 import { useRecoilValue, useRecoilState } from "recoil";
-import { allPortfolioAssets } from "../../../../atoms/PortfolioAssets";
+import {
+  allPortfolioAssets,
+  allPortfolioBoughtAssetsInStorage,
+} from "../../../../atoms/PortfolioAssets";
+import { SwipeListView } from "react-native-swipe-list-view";
+import { FontAwesome } from "@expo/vector-icons";
 
 const PortfolioAssetsList = () => {
   const navigation = useNavigation();
   const assets = useRecoilValue(allPortfolioAssets);
+  const [storageAssets, setStorageAssets] = useRecoilState(
+    allPortfolioBoughtAssetsInStorage
+  );
 
   const getCurrentBalance = () =>
     assets.reduce(
@@ -41,12 +50,44 @@ const PortfolioAssetsList = () => {
     );
   };
 
+  const onDeleteAsset = async (asset) => {
+    const newAssets = storageAssets.filter(
+      (coin) => coin.unique_id !== asset.item.unique_id
+    );
+    const jsonValue = JSON.stringify(newAssets);
+    await AsyncStorage.setItem("@portfolio_coins", jsonValue);
+    setStorageAssets(newAssets);
+  };
+
+  const renderDeleteButton = (data) => {
+    return (
+      <Pressable
+        style={{
+          flex: 1,
+          backgroundColor: "#EA3943",
+          alignItems: "flex-end",
+          justifyContent: "center",
+          paddingRight: 30,
+          marginLeft: 20,
+        }}
+        onPress={() => onDeleteAsset(data)}
+      >
+        <FontAwesome name="trash-o" size={24} color="white" />
+      </Pressable>
+    );
+  };
+
   const isChangePositive = () => getCurrentValueChange() >= 0;
 
   return (
-    <FlatList
+    <SwipeListView
       data={assets}
       renderItem={({ item }) => <PortfolioAssetItem assetItem={item} />}
+      rightOpenValue={-75}
+      disableRightSwipe
+      closeOnRowPress
+      keyExtractor={({ id }, index) => `${id}${index}`}
+      renderHiddenItem={(data) => renderDeleteButton(data)}
       ListHeaderComponent={
         <>
           <View style={styles.balanceContainer}>
